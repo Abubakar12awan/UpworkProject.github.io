f11bc68f55562c98b2e2da27718f839a1b25caaf
Abubakar <www.malikabubakar123@gmail.com>
2022-08-22 11:53:11 +0500

    gg

---
+++ index.js
@@ -0,0 +1,14 @@
+// let my=document.getElementById("i11");
+// my.style.backgroundColor = "blue"
+
+// setTimeout(() => {
+//     my.style.color="green";
+//     my.style.display="inherit";
+//     my.style.transition
+
+    
+// }, 1000);
+
+
+
+
