e177cb155eda2acf8e17b52f0f8bae318c8e60ce
Abubakar <www.malikabubakar123@gmail.com>
2022-08-22 14:50:38 +0500

    rrrr

--- index.js
+++ index.js
@@ -10,5 +10,34 @@
 // }, 1000);
 
 
+// document.getElementById("i22").color="blue";
+//document.getElementById("i22").color="blue";
 
 
+// my.style.backgroundColor = "blue"
+// let my=document.getElementById("i11");
+
+// my.style.display="none";
+
+// setTimeout(() => {
+//    // my.style.backgroundColor="green";
+    
+//    // my.style.transition
+    
+    
+// }, 2000);
+document.getElementById("i1").style=" margin-left: 589px;margin-top: 519px;background-color: white;transition: ease 0.9s;width: 187px;border-radius: 7px;    transform: rotate(0deg);";
+// 
+// document.ge
+
+document.getElementById("ii1").style=" width: 100px;border: 2px solid red;display: block;margin-top: 502px;margin-left: 166px;;transition: ease 0.9s;border-radius: 2px;    transform: rotate(0deg);";
+
+
+// var width = document.getElementById(body).width();
+
+
+// document.getElementById(body).resize(function () {
+//     if (width <= 480) {
+//         document.getElementById('i1').addClass('srfc');
+//     }
+// });
