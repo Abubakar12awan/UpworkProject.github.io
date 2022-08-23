44760b5fda84bf0a15f5ea1f7bf5ec32d697869f
Abubakar <www.malikabubakar123@gmail.com>
2022-08-23 06:01:37 +0500

    gtyu

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
     
     
 // }, 2000);
-document.getElementById("i1").style=" margin-left: 589px;margin-top: 519px;background-color: #199747;transition: ease 0.9s;width: 187px;border-radius: 7px;    transform: rotate(0deg);";
+document.getElementById("i1").style="box-shadow: 3px 3px black; margin-left: 589px;margin-top: 634px;background-color: #199747;transition: ease 0.9s;width: 187px;border-radius: 7px;    transform: rotate(0deg);";
 // 
 // document.ge
 
